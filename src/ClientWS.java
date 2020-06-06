8dab3e7b2a994fa263483609020254a7a59cf78a
KitayDehbi <dehbi.ayoub.98@gmail.com>
2020-06-06 13:10:48 +0100

    Initial commit

---
+++ src/ClientWS.java
@@ -0,0 +1,20 @@
+import ws.User;
+import ws.UserService;
+import ws.UserWS;
+
+import java.util.List;
+
+public class ClientWS {
+    public static void main(String[] args) {
+        UserService proxy = new UserWS().getUserServicePort();
+        System.out.println(proxy.getUser(2).getName());
+        System.out.println(proxy.getBirthYear(21));
+        List<User> list = proxy.getUserList();
+        System.out.println("--------------");
+        for (User u : list){
+            System.out.println(u.getName());
+            System.out.println(u.getAge());
+        }
+
+    }
+}
